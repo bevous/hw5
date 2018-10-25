25d861a6674fcf0317dacf40df854021bb25ad64
bevous <43658573+bevous@users.noreply.github.com>
2018-10-25 08:01:41 -0500

    ligic for addition added

--- Lewis_Nordan_hw5/Lewis_Nordan_hw5/largeint.cpp
+++ Lewis_Nordan_hw5/Lewis_Nordan_hw5/largeint.cpp
@@ -73,9 +73,47 @@ largeint& largeint::operator+(largeint& rh) {
 	largeint result;
 	int carryover = 0;
 	int resulTemp = 0;
+	vector<int> tempaddition;
 	string newint = "";
+	
+	
+	for (int x = 0; x < mydigits.size() || x < rh.getdigits().size(); x++) {
+		resulTemp = (mydigits[mydigits.size() - x] + rh.getdigits()[rh.getdigits().size() - x] + carryover);
+		tempaddition.push_back(resulTemp % 10);
+		carryover = resulTemp - tempaddition[tempaddition.size() - 1];
+		if (x++ == mydigits.size() || x++ == rh.getdigits().size() && carryover != 0) {
+			while (carryover != 0) {
+				if (x++ == mydigits.size()) {
+					resulTemp = rh.getdigits()[rh.getdigits().size() - x] + carryover;
+					tempaddition.push_back(resulTemp % 10);
+					carryover = resulTemp - tempaddition[tempaddition.size() - 1];
 
+					if (carryover == 0 && x++ < mydigits.size()) {
+						for (int y = x++; y < mydigits.size(); y++) {
+							tempaddition.push_back(mydigits[mydigits.size() - y]);
+						}
+					}
 
+				}
+				else if (x++ == rh.getdigits().size()) {
+					resulTemp = mydigits[mydigits.size() - x] + carryover;
+					tempaddition.push_back(resulTemp % 10);
+					carryover = resulTemp - tempaddition[tempaddition.size() - 1];
+
+					if (carryover == 0 && x++ < mydigits.size()) {
+						for (int y = x++; y < rh.getdigits().size(); y++) {
+							tempaddition.push_back(rh.getdigits()[rh.getdigits().size() - y]);
+						}
+					}
+
+				}
+			}
+		}
+	}
+	
+	for (int x = 0; x < tempaddition.size(); x++) {
+		newint += to_string(tempaddition[x]);
+	}
 
 	result.setInt(newint);
 }
